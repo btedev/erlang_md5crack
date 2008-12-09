77089e3a16ac7628c25f9105bbb2bb95d5ce6444
Barry Ezell <barrye@gmail.com>
2008-12-09 18:04:45 -0500

    distributed functions working

--- dist.erl
+++ dist.erl
@@ -14,12 +14,12 @@ stop() ->
 	rpc({stop_all}).
 
 % note: rpc:call below is a function on the rpc lib and has nothing to do with the homegrown rpc() above
-% rpc:call/2 allows a remote shell using the same password as the local shell, to make rpc calls on local functions
+% rpc:call/2 allows a remote shell using the same password as the local shell to make rpc calls on local functions
 register_me(Server, Nick) ->
-	rpc:call(Server, dist, enslave, [Nick]).
+	rpc:call(Server, dist, enlist, Nick).
 
-enslave(Nick) ->
-	Reply = rpc({enslave, Nick}),
+enlist(Nick) ->
+	Reply = rpc({enlist, Nick}),
 		case Reply of
 			ok ->
 				io:format("OK, I'm running as ~p on pid ~p...~n",[Nick, self()]),
@@ -38,7 +38,8 @@ parrot(Text) ->
 
 server_loop(Bots) ->
 	receive
-		{From, {enslave, Nick}} ->
+		{From, {enlist, Nick}} ->
+			io:format("Welcome ~p~n", [Nick]),
 			Bots2 = Bots:append(From, Nick),
 			From ! {dist, ok},
 			server_loop(Bots2);
@@ -48,8 +49,16 @@ server_loop(Bots) ->
 			server_loop(Bots);
 		{From, {decrypt, Crypted, Len}} ->
 			Pairs = pwd:partition_alphabet(Len, dict:size(Bots)),
-			distribute_work(dict:to_list(Bots), Pairs, Len),
+			distribute_work(dict:to_list(Bots), Pairs, Crypted, Len),
 			From ! {dist, ok},
+			server_loop(Bots);
+		{From, {found, Plain}} ->
+			io:format("~p decrypted password: ~p~n",[botnick(Bots,From),Plain]),
+			From ! {dist, you_rock},
+			server_loop(Bots);
+		{From, {notfound}} ->
+			io:format("Failed~n",[]),
+			From ! {dist, thanks_for_playing},
 			server_loop(Bots);
 		{From, {stop_all}} ->
 			dict:map(fun(Pid,Nick) -> Pid ! {bye} end, Bots),
@@ -59,18 +68,22 @@ server_loop(Bots) ->
 			server_loop(Bots)
 	end.
 
-distribute_work([],[],_) -> ok;
-distribute_work(BotList, Pairs, Len) ->
+distribute_work([],[],_,_) -> ok;
+distribute_work(BotList, Pairs, Crypted, Len) ->
 	[CurPair|RestPairs] = Pairs,
 	[{Pid,Nick}|RestBots] = BotList,
-	Pid ! {decrypt, CurPair, Len},
-	distribute_work(RestBots,RestPairs,Len).
+	Pid ! {decrypt, CurPair, Crypted, Len},
+	distribute_work(RestBots,RestPairs,Crypted,Len).
+
+botnick(Bots,Pid) ->
+	{ok, Nick} = dict:find(Pid,Bots),
+	Nick.
 
 client_loop() ->
 	receive
-		{decrypt, {First, Last}, Len} ->
-			io:format("Working on decrypting string of length ~p...~n",[Len]),
-
+		{decrypt, {Min, Max}, Crypted, Len} ->
+			io:format("Attempting decryption of string of length ~p...~n",[Len]),
+			analyze(Crypted, Min, Max),
 			client_loop();
 		{parrot, Text, Nick} ->
 			io:format("My name is ~p and I go ~p~n", [Nick,Text]),
@@ -81,3 +94,18 @@ client_loop() ->
 			io:format("Client cannot handle ~p~n",[Other]),
 			client_loop()
 	end.
+
+analyze(Crypted, Max, Max) ->
+   case erlang:md5(Max) of
+       Crypted ->
+           rpc({found, Max});
+       _ ->
+           rpc({notfound})
+   end;
+analyze(Crypted, Cur, Max) ->
+   case erlang:md5(Cur) of
+       Crypted ->
+           rpc({found, Cur});
+       _ ->
+           analyze(Crypted, pwd:next(Cur), Max)
+   end.
